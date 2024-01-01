7201fa312979a549fd94d28f39ea10d07e3cd480
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-01 19:14:24 +0530

    revistion

--- fragamnets/src/App.jsx
+++ fragamnets/src/App.jsx
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import "./App.css";
 function App() {
+  //const itemlist=[]
   const itemlist = [
     "Dal",
     "green Vegitable",
@@ -8,12 +10,17 @@ function App() {
     "soyabeen",
     "aur bhi bhut sare",
   ];
-
+// itw work when the arrys is empty 
+  if (itemlist == 0) {
+    return <>i am so bhokha </>;
+    
+  }
   //isase loop chala kr kaam kr skte hy isme bar bar list
   //nhi bnani pdti
   return (
     <React.Fragment>
       <h1>Helth item list </h1>
+    {itemlist.length===0 && <h2>i am also bhoka 2 </h2>}
       <ul className="list-group">
         {itemlist.map((item) => (
           <li className="list-group-item">{item}</li>
